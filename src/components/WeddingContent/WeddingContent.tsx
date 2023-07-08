346a9be5db840a028d00e4bc7e4b254a3c64ecaa
Sean Coding <hustle0985@gmail.com>
2023-07-08 16:54:37 +0800

    video update

--- src/components/WeddingContent/WeddingContent.tsx
+++ src/components/WeddingContent/WeddingContent.tsx
@@ -6,13 +6,21 @@ import WeddingForm from '../WeddingForm/WeddingForm';
 import IFrame from '../IFrame/IFrame'
 import {Canvas} from '@react-three/fiber'
 
-const VideoMesh = () => {
-  const texture = useVideoTexture("./video/bg-pc.MP4")
+interface IVideoMesh {
+  videoSrc: string,
+  onClick: () => void,
+  x: number,
+}
+
+const VideoMesh = ({videoSrc, onClick, x}:IVideoMesh) => {
+  const texture = useVideoTexture(videoSrc)
   return (
-    <mesh position-y={ -1 } rotation={[- Math.PI * .5 , 0 , 0]} scale={ 2 }>
-      <planeGeometry />
-      <meshBasicMaterial map={texture} toneMapped={false} />
-    </mesh>  
+    <Float floatingRange={[0, 1]} speed={1.1} onClick={onClick} rotationIntensity={5}>
+      <mesh position={[x, 0, 0]} scale={.3}>
+          <sphereGeometry />
+          <meshBasicMaterial map={texture} toneMapped={false} />
+      </mesh>
+    </Float>
   )
 }
 
@@ -35,35 +43,14 @@ const WeddingContent = () => {
       <Canvas>
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
-        <Float floatingRange={[0, .5]} floatIntensity={0}>
-          <mesh position={[0, 0, 0]} scale={.3} onClick={()=> {showModal('婚禮資訊', <p>圖片</p>)}} onPointerEnter={()=> {console.log('???')}}>
-              <sphereGeometry />
-              <meshLambertMaterial color="#d4cea1" />
-          </mesh>
-        </Float>
-        <Float floatingRange={[0, .5]} speed={0.9}>
-          <mesh position={[1, 0, 0]} scale={.3} onClick={()=> {showModal('婚禮表單', <WeddingForm />)}}>
-              <sphereGeometry />
-              <meshLambertMaterial color="#b7cbbf" />
-          </mesh>
-        </Float>
-        <Float floatingRange={[0, 0.5]} speed={1.1}>
-          <mesh position={[-1, 0, 0]} scale={.3} onClick={()=> {showModal('Playlist', <IFrame/>)}}>
-              <sphereGeometry />
-              <meshLambertMaterial color="#536040" />
-          </mesh>
-        </Float>
-        <VideoMesh />
-        <Sparkles></Sparkles>
+        <Sparkles />
         <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+        <VideoMesh videoSrc="./video/0.MP4" onClick={()=> {showModal('PlayList', <IFrame />)}} x={-1}/>
+        <VideoMesh videoSrc="./video/1.MP4" onClick={()=> {showModal('婚禮表單', <WeddingForm />)}} x={0}/>
+        <VideoMesh videoSrc="./video/2.MP4" onClick={()=> {showModal('婚禮資訊', <p>圖片</p>)}} x={1}/>
       </Canvas>
       
       <Title>Sean & Chloe's Wedding</Title>
-      {/* <ButtonContainer>
-        <CustomButton icon={<CalendarOutlined />} ghost={true} onClick={()=> {showModal('婚禮資訊', <p>圖片</p>)}} />
-        <CustomButton icon={<FormOutlined />} ghost={true} onClick={()=> {showModal('婚禮表單', <WeddingForm />)}} />
-        <CustomButton icon={<YoutubeOutlined />} ghost={true} onClick={()=> {showModal('Playlist', <IFrame/>)}} />
-      </ButtonContainer> */}
       <Modal title={modalTitle} open={isModalOpen} onCancel={closeModal} footer={null}>
         {modalContent}
       </Modal>
